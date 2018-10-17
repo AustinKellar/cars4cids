bc152e30fe9b9669b459eaa49731187659a17cd3
Austin Kellar <ajk@eduroam-169-233-142-126.ucsc.edu>
2018-10-17 15:13:19 -0700

    refactored player script

--- Assets/Scripts/TankMovement.cs
+++ Assets/Scripts/TankMovement.cs
@@ -2,49 +2,29 @@
 
 public class TankMovement : MonoBehaviour
 {
-    public float m_Speed = 30f;                 // How fast the tank moves forward and back.
-    public float m_TurnSpeed = 180f;            // How fast the tank turns in degrees per second.
-    public float maxSpeed = 30f;
+    public float speed;
+    public float minSpeed;
+    public float maxSpeed;
+    public float turnSpeed;            
 
-    private string m_MovementAxisName;          // The name of the input axis for moving forward and back.
-    private string m_TurnAxisName;              // The name of the input axis for turning.
-    private Rigidbody m_Rigidbody;              // Reference used to move the tank.
-    private float m_MovementInputValue;         // The current value of the movement input.
-    private float m_TurnInputValue;             // The current value of the turn input.
-
-
-    private void Awake()
-    {
-        m_Rigidbody = GetComponent<Rigidbody>();
-    }
-
-
-    private void OnDisable()
-    {
-        // When the tank is turned off, set it to kinematic so it stops moving.
-        m_Rigidbody.isKinematic = true;
-    }
-
+    private Rigidbody _rigidbody;             
+    private float _movementInput;         
+    private float _turnInput;            
 
     private void Start()
     {
-        // The axes names are based on player number.
-        m_MovementAxisName = "Vertical";
-        m_TurnAxisName = "Horizontal";
-
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
 
     private void Update()
     {
-        // Store the value of both input axes.
-        m_MovementInputValue = Input.GetAxis(m_MovementAxisName);
-        m_TurnInputValue = Input.GetAxis(m_TurnAxisName);
+        _movementInput = Input.GetAxis("Vertical");
+        _turnInput = Input.GetAxis("Horizontal");
     }
 
     private void FixedUpdate()
     {
-        // Adjust the rigidbodies position and orientation in FixedUpdate.
         Move();
         Turn();
     }
@@ -52,51 +32,23 @@ public class TankMovement : MonoBehaviour
 
     private void Move()
     {
-        // Create a vector in the direction the tank is facing with a magnitude based on the input, speed and the time between frames.
-        if (m_MovementInputValue == 1)
+        if (_movementInput == 1 && speed < maxSpeed) // if pressing forward, increase speed
         {
-            if (m_Speed < 55)
-            {
-                m_Speed++;
-                Vector3 movement = transform.forward * m_Speed * Time.deltaTime;
-                m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
-            } else
-            {
-                Vector3 movement = transform.forward * m_Speed * Time.deltaTime;
-                m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
-            }
-        } 
-        else if (m_MovementInputValue == -1)
-        {
-            if (m_Speed > maxSpeed)
-            {
-                m_Speed--;
-                Vector3 movement = transform.forward * m_Speed * Time.deltaTime;
-                m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
-            }
-            else
-            {
-                Vector3 movement = transform.forward * m_Speed * Time.deltaTime;
-                m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
-            }
-        } 
-        else
-        {
-            Vector3 movement = transform.forward * m_Speed * Time.deltaTime;
-            m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
+            speed++;   
         }
+        else if (_movementInput == -1 && speed > minSpeed) // if pressing back, decrease speed
+        {
+            speed--;
+        }
+
+        _rigidbody.velocity = transform.forward * speed;
     }
 
 
     private void Turn()
     {
-        // Determine the number of degrees to be turned based on the input, speed and time between frames.
-        float turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
-
-        // Make this into a rotation in the y axis.
+        float turn = _turnInput * turnSpeed * Time.deltaTime;
         Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
-
-        // Apply this rotation to the rigidbody's rotation.
-        m_Rigidbody.MoveRotation(m_Rigidbody.rotation * turnRotation);
+        _rigidbody.MoveRotation(_rigidbody.rotation * turnRotation);
     }
 }
