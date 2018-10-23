f3d37af860294c8308b61114a8771a354f26c87c
Tristian M <tgmorale@ucsc.edu>
2018-10-22 19:55:08 -0700

    Added NavigatorUI UI prefab with UIManager script. Added float fuel and float maxFuel to PlayerMovement

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public float _maxGaugeSize;
+
+    public PlayerMovement car;
+    public Text speedText;
+    public RectTransform fuelGauge;
+    public Button checkpointButton;
+    public Button manipulateButton;
+
+
+	void Start ()
+    {
+        car = GameObject.FindObjectOfType<PlayerMovement>();
+        _maxGaugeSize = fuelGauge.rect.width;
+        speedText.text = "Speed: " + car.speed;
+	}
+	
+	void Update ()
+    {
+        Debug.Log("fuel: " + car.fuel + " maxFuel: " + car.maxFuel);
+        fuelGauge.rect.Set(fuelGauge.rect.x, 
+                           fuelGauge.rect.y, 
+                           (car.fuel / car.maxFuel) * _maxGaugeSize, 
+                           fuelGauge.rect.height);
+        speedText.text = "Speed: " + car.speed;
+    }
+
+    public void Checkpoint()
+    {
+
+    }
+
+    public void Manipulate()
+    {
+
+    }
+}
