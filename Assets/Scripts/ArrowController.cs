041b0f0a7e2679f1eac81b5018c49c650f4296ca
Tristian M <tgmorale@ucsc.edu>
2018-10-23 23:15:11 -0700

    Added arrow mesh, ArrowController script, NavCam prefab and updated car prefab

---
+++ Assets/Scripts/ArrowController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowController : MonoBehaviour
+{
+    public GameObject goal;
+	
+	void Update ()
+    {
+        transform.LookAt(goal.transform);
+        Quaternion q = new Quaternion();
+        q.eulerAngles = new Vector3(0f, 0f, 0f) + gameObject.transform.rotation.eulerAngles;
+        gameObject.transform.rotation = q;
+    }
+}
